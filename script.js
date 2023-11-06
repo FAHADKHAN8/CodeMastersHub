ab6c372185e5141b7e72024cffe4341a66e12669
FAHAD KHAN <mohammadfahadkhan11@gmail.com>
2023-11-06 15:19:38 +0530

    js

--- script.js
+++ script.js
@@ -1,44 +1,17 @@
-window.onload = function () {
-    // Add smooth scrolling to navigation links
-    document.querySelectorAll('nav a').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
+document.addEventListener('DOMContentLoaded', function () {
+    var articles = document.querySelectorAll('.article h3');
+    articles.forEach(function (article) {
+        article.style.fontSize = '28px'; 
+        article.style.color = 'black'; 
+    });
 
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-
-            window.scrollTo({
-                top: targetElement.offsetTop - 100,
-                behavior: 'smooth'
-            });
+    var articleContainers = document.querySelectorAll('.article');
+    articleContainers.forEach(function (container) {
+        container.addEventListener('mouseenter', function () {
+            container.style.backgroundColor = '#f0f0f0'; 
         });
-
-        // Change the background color of the header
-        var header = document.querySelector('.header');
-        header.style.backgroundColor = '#4c074c';
-
-        // Add a class to the navigation links for better styling
-        var navLinks = document.querySelectorAll('.navbar a');
-        navLinks.forEach(function (link) {
-            link.classList.add('nav-link');
-        });
-
-        // Change the font size and color of featured articles
-        var articles = document.querySelectorAll('.article h3');
-        articles.forEach(function (article) {
-            article.style.fontSize = '28px'; // Increase font size
-            article.style.color = 'black'; // Change text color
-        });
-
-        // Add a hover effect to the featured articles
-        var articleContainers = document.querySelectorAll('.article');
-        articleContainers.forEach(function (container) {
-            container.addEventListener('mouseenter', function () {
-                container.style.backgroundColor = '#f0f0f0'; // Change background color on hover
-            });
-            container.addEventListener('mouseleave', function () {
-                container.style.backgroundColor = 'white'; // Revert background color on mouse leave
-            });
+        container.addEventListener('mouseleave', function () {
+            container.style.backgroundColor = 'white'; 
         });
     });
-};
+});

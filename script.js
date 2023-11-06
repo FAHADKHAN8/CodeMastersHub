2c7d01ac028dcf52de10b909e2cb32f0b36f4cb9
FAHAD KHAN <mohammadfahadkhan11@gmail.com>
2023-11-06 16:04:43 +0530

    popup func

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-
     const header = document.querySelector(".header");
     const changeColorButton = document.getElementById("changeColorButton");
-
+    const popup = document.getElementById("popup");
 
     function changeHeaderColor() {
-
         const randomColor = getRandomColor();
         header.style.backgroundColor = randomColor;
+        updatePopupText("Navigation bar color changed!");
     }
 
     function getRandomColor() {
@@ -19,6 +18,13 @@ document.addEventListener("DOMContentLoaded", function () {
         return color;
     }
 
+    function updatePopupText(text) {
+        popup.textContent = text;
+        popup.style.display = "block";
+        setTimeout(function () {
+            popup.style.display = "none";
+        }, 2000); 
+    }
 
     if (changeColorButton) {
         changeColorButton.addEventListener("click", changeHeaderColor);
